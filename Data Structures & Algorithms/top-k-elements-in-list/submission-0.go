7e86788640394f6715f0b6ca450b4c5000f8a7b5
Vortex <74016856+Vortex-art-01@users.noreply.github.com>
2026-07-09 09:43:56 +0500

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.go
@@ -0,0 +1,29 @@
+func topKFrequent(nums []int, k int) []int {
+	freqs := map[int]int{}
+	for _, val := range nums {
+		freqs[val]++
+	}
+
+	res := []int{}
+    fmt.Println(freqs)
+	
+    for i := 0; i < k; i++ {
+        first := true
+        max := []int{}
+
+        for num, freq := range freqs {
+            if first || freq > max[1] {
+                max = []int{num, freq}
+            }
+            first = false
+        }
+
+        if len(max) != 0 {
+            delete(freqs, max[0])
+        }
+
+        res = append(res, max[0])
+    }
+
+	return res
+}
