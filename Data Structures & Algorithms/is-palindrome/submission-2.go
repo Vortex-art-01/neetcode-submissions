e36b34d15ba0f533008a6aa494b56ee54aabab8f
Vortex <74016856+Vortex-art-01@users.noreply.github.com>
2026-07-27 09:25:35 +0500

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.go
@@ -0,0 +1,27 @@
+func isPalindrome(s string) bool {
+	fmt.Println(s)
+	prerared_s := strings.ReplaceAll(strings.ToLower(s), " ", "")
+	prerared_s = strings.ReplaceAll(prerared_s, "?", "")
+	prerared_s = strings.ReplaceAll(prerared_s, ".", "")
+	prerared_s = strings.ReplaceAll(prerared_s, "!", "")
+	prerared_s = strings.ReplaceAll(prerared_s, "/", "")
+	prerared_s = strings.ReplaceAll(prerared_s, ",", "")
+	prerared_s = strings.ReplaceAll(prerared_s, "'", "")
+	prerared_s = strings.ReplaceAll(prerared_s, ":", "")
+	fmt.Println("prerared_s", prerared_s)
+
+	l, r := 0, len(prerared_s)-1
+
+	for l <= r {
+		if prerared_s[l] != prerared_s[r] {
+		fmt.Println("error")
+		fmt.Println("s[l]", string(s[l]))
+		fmt.Println("s[r]", string(s[r]))
+			
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
